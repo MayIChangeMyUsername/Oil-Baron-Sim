8f5da719da43e6d95253b3d9e81866453d21edc5
Polonity <polonityofficial@gmail.com>
2023-01-13 13:38:34 +0100

    Button work, YES!

---
+++ Assets/Antons Scripts/TestButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestButton : MonoBehaviour
+{
+    public  Button yourButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        GameManager.week++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
