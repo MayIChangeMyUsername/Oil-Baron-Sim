ddbcbb56c5a2e675918b1a384eb8382f191b4905
Polonity <polonityofficial@gmail.com>
2023-01-13 11:20:56 +0100

    click yes work(save me)

--- Assets/Antons Scripts/SkipToNextDay.cs
+++ Assets/Antons Scripts/SkipToNextDay.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkipToNextDay : MonoBehaviour
 {
+    [SerializeField]
+    KeyCode mouseOnClick;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        STND();
+    }
+    void STND()
+    {
+        if (Input.GetKeyDown(mouseOnClick))
+        {
+            GameManager.week++;
+        }
     }
 }
