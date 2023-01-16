c03d921ae1d3922a410b4daa940e3e9deea7d481
MayIChangeMyUsername <elliot.rg.persson@gmail.com>
2023-01-16 15:29:14 +0100

    WIP event texter i script

--- Assets/Elliots Script/Eventtext.cs
+++ Assets/Elliots Script/Eventtext.cs
@@ -43,11 +43,21 @@ public class Eventtext : MonoBehaviour
 
         eventText = GameObject.Find("Eventtext").GetComponent<TextMeshProUGUI>();
 
-        eventArray[0] = "Would you like to build a new factory?";
+        eventArray[0] = "Consumers want lower prices, do you want to reduce prices?";
 
+        eventArray[1] = "";
 
+        eventArray[2] = "Stock market crashes!";
 
+        eventArray[3] = "Bad rumours are spreading about your corporation";
 
+        eventArray[4] = "An oil pipeline is leaking";
+
+        eventArray[5] = "Stocks rise in value and sales are increasing";
+
+        eventArray[6] = "You have gained traction on social media and more people buy oil";
+
+        eventArray[7] = "A more effective way of extracting oil has been invented";
     }
 
 
