25d0e717ba12271e746706971f573991716e1d20
Gren-arch <axelsvanstroms@gmail.com>
2023-01-19 10:09:35 +0100

    hourglass animation fix

--- Assets/HourglassTrigger.cs
+++ Assets/HourglassTrigger.cs
@@ -1,27 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HourglassTrigger : MonoBehaviour
 {
-    public Button m_YourFirstButton;
+    
     Animator myanimator;
+    public Button button;
+        
 
     const string Trigger_ANIM = "Trigger";
-    // Start is called before the first frame update
+    
+
     void Start()
     {
         myanimator = GetComponent<Animator>();
-        
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
 
     }
+
+   
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myanimator.SetTrigger(Trigger_ANIM);
         }
+        
 
+    }
+    void TaskOnClick()
+    {  
+     myanimator.SetTrigger(Trigger_ANIM); 
     }
 
 }
