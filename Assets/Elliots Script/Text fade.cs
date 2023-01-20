e53c9f21fc00e0cec68753f07d378f838ded6764
MayIChangeMyUsername <elliot.rg.persson@gmail.com>
2023-01-20 11:10:15 +0100

    text fade in

--- Assets/Elliots Script/Text fade.cs
+++ Assets/Elliots Script/Text fade.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
 public class Textfade : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
